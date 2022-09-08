a5260421a38cc5dbb1f20a219c4d87f0f367cc59
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-09-08 16:36:32 -0400

    create function to get fingerprin

---
+++ src/lib/getFingerprint.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import info from "./getFingerprint.lib";
+
+const HASH_KEY = "E86E2612010258B35137";
+
+/**
+ * Function to get browser finger print
+ * @returns
+ */
+const getFingerprint = () => {
+  let HASH_KEY_CUSTOM = HASH_KEY;
+
+  // If Cypress is installed, then load env from cypress, adding support for cypress
+  if (typeof Cypress != "undefined") HASH_KEY_CUSTOM = Cypress.env("SECURE_DATA_HASH_KEY") || Cypress.env("REACT_APP_SECURE_DATA_HASH_KEY") || HASH_KEY;
+  //Load the custom HASH KEY from process.env
+  else HASH_KEY_CUSTOM = process.env.SECURE_DATA_HASH_KEY || process.env.REACT_APP_SECURE_DATA_HASH_KEY || HASH_KEY;
+
+  if (typeof window === "undefined") return HASH_KEY_CUSTOM;
+  return info.getFingerprint() + HASH_KEY_CUSTOM;
+};
+
+export default getFingerprint;
